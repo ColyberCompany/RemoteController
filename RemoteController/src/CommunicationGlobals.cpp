42e78ecf91355ccadb0c24711237f8984017b6e6
Jan Wielgus <jan.wielgus12@gmail.com>
2021-03-28 18:57:45 +0200

    Bug fix: files was linked in a wronng order
    It turned out that data packet instances was linked before commData and it caused program failure before main()
    I removed separate cpp files and instantiated both in a correct order in CommunicationGlobals.cpp file

--- RemoteController/src/CommunicationGlobals.cpp
+++ RemoteController/src/CommunicationGlobals.cpp
@@ -1,11 +1,16 @@
 /**
- * @file DataPackets.cpp
+ * @file CommunicationGlobals.cpp
  * @author Jan Wielgus (jan.wielgus12@gmail.com)
+ * @brief Instantiation of communication global variables.
  * @date 2021-03-28
  * 
  */
 
+#include "../Communication/CommData.h"
 #include "../Communication/DataPackets.h"
+
+
+CommData commData;
 
 
 namespace DataPackets
@@ -15,4 +20,5 @@ namespace DataPackets
     Steering steering;
     FlightModeChange flightModeChange;
     DroneMeasurementsAndState droneMeasurementsAndState;
+    // all other data packets...
 }
