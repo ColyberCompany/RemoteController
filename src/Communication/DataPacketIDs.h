a96926d77b39174d52403da220046277a5ae777c
Jan Wielgus <jan.wielgus12@gmail.com>
2022-07-04 13:40:21 +0200

    Added DataPacketIDs.h

---
+++ src/Communication/DataPacketIDs.h
@@ -0,0 +1,28 @@
+/**
+ * @file DataPacketIDs.h
+ * @author Jan Wielgus
+ * @brief This file contains IDs for all DataPackets used by drone.
+ * @date 2022-07-04
+ */
+
+#ifndef DATAPACKETSIDS_h
+#define DATAPACKETSIDS_h
+
+#include <cstdint>
+
+
+namespace DataPacketIDs {
+    #define PacketIDType constexpr inline uint16_t
+
+    PacketIDType steering               = 0;
+    PacketIDType flightModeChange       = 1;
+    PacketIDType pidTuning              = 2;
+    PacketIDType droneMeasurements      = 50;
+    PacketIDType droneState             = 51;
+
+    // all packets IDs have to be created here with the name of data packet itself
+    #undef PacketIDType
+}
+
+
+#endif
