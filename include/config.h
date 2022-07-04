c5bf9e5913321774497ee10f008b5b12030521b4
Jan Wielgus <jan.wielgus12@gmail.com>
2022-07-04 17:38:55 +0200

    Updated config.h

--- include/config.h
+++ include/config.h
@@ -2,44 +2,41 @@
  * @file config.h
  * @author Jan Wielgus
  * @brief Configuration file.
- * @version 0.1
  * @date 2020-12-01
- * 
- * @copyright Copyright (c) 2020
- * 
  */
 
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#ifdef ARDUINO
-    #include <Arduino.h>
-#endif
+#include <cstdint>
+#include <cstddef>
+#include <IPAddress.h>
 
-#include "Enums/BaudRateTypes.h"
+#define CI constexpr inline
 
 
 namespace Config
 {
-    const uint16_t MainFrequency_Hz = 250;
-    const uint16_t MainInterval_us = 1000000 / MainFrequency_Hz;
-    const float MainInterval_s = 1.f / MainFrequency_Hz; // delta time between next main loop executions
+    CI uint16_t MainFrequency_Hz = 250;
+    CI uint16_t MainInterval_us = 1000000 / MainFrequency_Hz;
+    CI float MainInterval_s = 1.f / MainFrequency_Hz; // delta time between next main loop executions
 
-    const uint8_t MaxSimpleTaskerTasks = 15;
+    CI uint8_t MaxSimpleTaskerTasks = 15;
 
-    const size_t DroneCommMaxBufferSize = 40;
-    const uint16_t DroneCommReceivingFrequency_Hz = 20; // TODO: set receiving frequency
-    const uint16_t DroneCommSteeringSendingFrequency_Hz = 20;
+    CI size_t DroneCommMaxBufferSize = 40;
+    CI uint16_t DroneCommReceivingFrequency_Hz = 20; // TODO: set receiving frequency
+    CI uint16_t DroneCommSteeringSendingFrequency_Hz = 20;
 
-    const uint8_t LeftSwitchPin = D8;
-    const uint8_t RightSwitchPin = D7;
+    CI uint8_t LeftSwitchPin = D8;
+    CI uint8_t RightSwitchPin = D7;
 
-    const uint16_t ControlSticksInputOffset = 450;
+    CI uint16_t ControlSticksInputOffset = 450;
 
-    const char* const WiFiSSID = "HUAWEI_P_smart";
-	const char* const WiFiPassword = "22222222";
-	const uint16_t WiFiPort = 8888;
+    CI char* const WiFiSSID = "HUAWEI_P_smart";
+	CI char* const WiFiPassword = "22222222";
+	CI uint16_t WiFiPort = 8888;
 }
 
+#undef CI
 
 #endif
